f1c6b632a4d91ddadc3e70718d19e344c6def529
az <253670756@qq.com>
2021-08-04 17:53:12 +0800

    1.init

---
+++ src/main/java/com/az/pojo/User.java
@@ -0,0 +1,13 @@
+package com.az.pojo;
+
+/**
+ * @PC : AZ_DELL
+ * @Author : az
+ * @e-mail : 253670756@qq.com
+ * @Date : 2021/8/4 17:37
+ * @Version: 1.0
+ */
+public class User {
+    private String name;
+    private int age;
+}
